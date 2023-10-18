ff5b6394ffcd4e8ec80b88ed2ab66823e2328027
konstantgr <konstantin.grotov@gmail.com>
2023-10-18 10:33:27 +0200

    fix order of experiments

--- wirenec_optimization/optimize.py
+++ wirenec_optimization/optimize.py
@@ -1,23 +1,25 @@
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 
 from wirenec_optimization.experiment import (
     MultiSeedOptimizationExperiment,
     SingleOptimizationExperiment,
 )
 
+
 if __name__ == "__main__":
     config = OmegaConf.load("configs/single_layers_experiment.yaml")
     # config = OmegaConf.load("configs/single_spatial_experiment.yaml")
-    # config = OmegaConf.load("configs/multi_seed_experiment.yaml")
 
     experiment = SingleOptimizationExperiment(config)
-    # experiment = MultiSeedOptimizationExperiment(config)
 
     experiment.run()
     experiment.save_results()
 
-    save_each_iteration = True
-    # experiment.run(save_each_iteration=save_each_iteration)
+    # config = OmegaConf.load("configs/multi_seed_experiment.yaml")
+    # experiment = MultiSeedOptimizationExperiment(config)
 
-    if not save_each_iteration:
-        experiment.save_results()
+    # save_each_iteration = True
+    # experiment.run(save_each_iteration=save_each_iteration)
+    #
+    # if not save_each_iteration:
+    #     experiment.save_results()
